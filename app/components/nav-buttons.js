81a2763e3ae55d13d8175914eb39c7530e399994
Meghan <mmhade@gmail.com>
2017-08-07 15:33:38 -0700

    add nav-buttons script and template

--- app/components/nav-buttons.js
+++ app/components/nav-buttons.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	next: null,
+	back: null,
+	actions: {
+		sendChangeRoute: function(route){
+			this.sendAction('sendChangeRoute', route);
+		}
+	}
 });
