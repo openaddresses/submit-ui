1e97bf3ea304d664192a91fe547df63dcd6ac427
Meghan <mmhade@gmail.com>
2017-08-21 16:31:10 -0700

    add data format selection

--- app/controllers/data.js
+++ app/controllers/data.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 import sharedActions from '../mixins/shared-actions';
 
-
 export default Ember.Controller.extend(sharedActions, {
+	actions: {
+		selectDataFormat: function(dataFormat){
+			console.log('selected ' + dataFormat);
+		}
+	}
 });
