5e96c3cb59c487ed1e16218023a175a746c237f5
Meghan <mmhade@gmail.com>
2017-08-21 16:43:51 -0700

    dataformat option buttons hold state

--- app/controllers/data.js
+++ app/controllers/data.js
@@ -2,9 +2,11 @@ import Ember from 'ember';
 import sharedActions from '../mixins/shared-actions';
 
 export default Ember.Controller.extend(sharedActions, {
+	dataFormat: null,
+
 	actions: {
 		selectDataFormat: function(dataFormat){
-			console.log('selected ' + dataFormat);
+			this.set('dataFormat', dataFormat);
 		}
 	}
 });
