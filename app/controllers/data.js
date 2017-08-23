7fa9898709b398f8098f9c0b1a2092941b164456
Meghan <mmhade@gmail.com>
2017-08-23 16:50:13 -0700

    add data format drop-down

--- app/controllers/data.js
+++ app/controllers/data.js
@@ -4,6 +4,8 @@ import sharedActions from '../mixins/shared-actions';
 export default Ember.Controller.extend(sharedActions, {
 	dataFormat: null,
 	proceed: false,
+	formats: ["Direct upload", "ArcGIS server link", "Download link (.zip, etc.)"],
+	selectedFormat: null,
 
 	actions: {
 		selectDataFormat: function(dataFormat){
