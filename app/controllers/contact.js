1e63691e03c84a51caba186e08fdcea59066052d
Meghan <mmhade@gmail.com>
2017-08-30 16:15:03 -0700

    maintainer contact info UI

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -3,4 +3,25 @@ import sharedActions from '../mixins/shared-actions';
 
 
 export default Ember.Controller.extend(sharedActions, {
+	name: null,
+	email: null,
+	emailMissing: false,
+	emailError: Ember.computed('emailMissing', 'email', function(){
+		if (this.get('emailMissing') === true && this.get('email') === null){
+			return true;
+		} else {
+			return false;
+		}
+	}),
+
+	actions: {
+		changeRoute: function(route){
+			if (this.get('email')) {
+				this.transitionToRoute(route);
+			} else {
+				(this.set('emailMissing', true));
+			}
+		}
+	}
+
 });
