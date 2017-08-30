07c7d8a7d95c2a9db0a2c626a5bf02580af42228
Meghan <mmhade@gmail.com>
2017-08-30 12:56:03 -0700

    license drop down selection

--- app/controllers/license.js
+++ app/controllers/license.js
@@ -3,4 +3,32 @@ import sharedActions from '../mixins/shared-actions';
 
 
 export default Ember.Controller.extend(sharedActions, {
+	selected_license: null,
+	licenses: [
+		{
+			"name": "license 1",
+			"text": "all the rules"
+		},
+		{
+			"name": "license 2",
+			"text": "some of the rules"
+		}
+	],
+	licensePresent: null,
+	actions: {
+		selectLicense: function(license){
+			this.set('selectedLicense', license);
+		},
+		changeRoute: function(route){
+			if (this.get('license')){
+				this.set('licensePresent', true)
+			} else {
+				console.log("test")
+				this.set('licensePresent', false)
+			}
+			if (this.get('licensePresent') == true) {
+				this.transitionToRoute(route);
+			}
+		}
+	}
 });
