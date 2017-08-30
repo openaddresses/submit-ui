cdf862e9381777ea43e5a36a1fcae94e460711a6
Meghan <mmhade@gmail.com>
2017-08-30 11:29:34 -0700

    add UI for source and frequency page

--- app/controllers/data.js
+++ app/controllers/data.js
@@ -4,10 +4,13 @@ import sharedActions from '../mixins/shared-actions';
 export default Ember.Controller.extend(sharedActions, {
 	formats: ["Direct upload", "ArcGIS server link", "Download link (.zip, etc.)"],
 	selectedFormat: null,
+	formatChange: Ember.computed('selectedFormat', function(){
+		this.set('data', null);
+	}),
 	data: null,
-	proceed: Ember.computed('data', function(){
+	proceed: Ember.computed('data', function(input){
 		// file validation here?
-		if (this.get('data')) {
+		if (this.get('data') !== null && this.get('data') !== "") {
 			return true;
 		} else {
 			return false;
@@ -15,7 +18,11 @@ export default Ember.Controller.extend(sharedActions, {
 	}),
 	actions: {
 		updateData(input){
-			console.log(input)
+			this.set('data', input);
+		},
+		updateFormat(input){
+		},
+		uploadData(input){
 		}
 	}
 });
--- app/controllers/source.js
+++ app/controllers/source.js
@@ -3,4 +3,32 @@ import sharedActions from '../mixins/shared-actions';
 
 
 export default Ember.Controller.extend(sharedActions, {
+	source: null,
+	frequency: null,
+	sourcePresent: null,
+	frequencyPresent: null,
+
+	actions: {
+		setDataSource: function(source){
+			console.log(source)
+		},
+		setUpdateFrequency: function(frequency){
+			console.log(frequency)
+		},
+		changeRoute: function(route){
+			if (this.get('source')){
+				this.set('sourcePresent', true)
+			} else {
+				this.set('sourcePresent', false)
+			}
+			if (this.get('frequency')){
+				this.set('frequencyPresent', true)
+			} else {
+				this.set('frequencyPresent', false)
+			}
+			if (this.get('frequencyPresent') == true && this.get('sourcePresent') == true){
+				this.transitionToRoute(route);
+			}
+		}
+	}
 });
