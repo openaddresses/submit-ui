017e07660a92b1f8ee199042f12261d2bad3a5ec
Meghan <mmhade@gmail.com>
2017-09-01 12:50:48 -0700

    update source page logic and edit review page text

--- app/controllers/source.js
+++ app/controllers/source.js
@@ -5,30 +5,34 @@ import sharedActions from '../mixins/shared-actions';
 export default Ember.Controller.extend(sharedActions, {
 	source: null,
 	frequency: null,
-	sourcePresent: null,
-	frequencyPresent: null,
+	sourceMissing: false,
+	frequencyMissing: false,
+	sourceError: Ember.computed('source', 'sourceMissing', function(){
+		if (this.get('sourceMissing') === true && this.get('source') === null){
+			return true;
+		} else {
+			return false;
+		}
+	}),
+	frequencyError: Ember.computed('frequency', 'frequencyMissing', function(){
+		if (this.get('frequencyMissing') === true && this.get('frequency') === null){
+			return true;
+		} else {
+			return false;
+		}
+	}),
 
 	actions: {
-		setDataSource: function(source){
-			console.log(source)
-		},
-		setUpdateFrequency: function(frequency){
-			console.log(frequency)
-		},
 		changeRoute: function(route){
-			if (this.get('source')){
-				this.set('sourcePresent', true)
-			} else {
-				this.set('sourcePresent', false)
-			}
-			if (this.get('frequency')){
-				this.set('frequencyPresent', true)
-			} else {
-				this.set('frequencyPresent', false)
-			}
-			if (this.get('frequencyPresent') == true && this.get('sourcePresent') == true){
+			if (this.get('source') && this.get('frequency')){
 				this.transitionToRoute(route);
-			}
+			} else if (this.get('sourceError')){
+				this.set('sourceMissing', true)
+			} else if (this.get('frequencyError')){
+				this.set('frequencyMissing', true)
+			} 
+
+			
 		}
 	}
 });
