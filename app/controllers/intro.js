e4178f99e1a028a403dc824d6fde2ab87d1db34f
Meghan <mmhade@gmail.com>
2017-09-14 16:09:50 -0700

    add country drop-down

--- app/controllers/intro.js
+++ app/controllers/intro.js
@@ -3,4 +3,20 @@ import sharedActions from '../mixins/shared-actions';
 
 
 export default Ember.Controller.extend(sharedActions, {
+	// these are placeholders, country and region data will be supplied through api
+	countries: [{title: 'United States'},{title: 'Canada'}],
+	// regions:
+	selectedCountry: null,
+	// selectedRegion: null,
+	
+	actions: {
+		selectCountry: function(selected){
+			this.set('selectedCountry', selected);
+			console.log(this.get('selectedCountry').title)
+		},
+		// selectRegion: function(selected){
+		// 	this.set('selectedRegion', selected);
+		// 	console.log(this.get('selectedRegion').title)
+		// }
+	}
 });
