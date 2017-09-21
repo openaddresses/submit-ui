eac31de532138f4e02410df443f77e41468422a6
Meghan <mmhade@gmail.com>
2017-09-21 15:09:06 -0700

    add mirage data

--- app/controllers/intro.js
+++ app/controllers/intro.js
@@ -5,12 +5,29 @@ import sharedActions from '../mixins/shared-actions';
 export default Ember.Controller.extend(sharedActions, {
 	// these are placeholders, country and region data will be supplied through api
 	countries: [{title: 'United States'},{title: 'Canada'}],
+	// countries: Ember.computed(function(){
+	// 	Ember.$.ajax({
+	// 		type: "GET",
+ //    	contentType: "text/xml",
+	// 		url: 'api/countries'
+	// 	}).then(function(response){
+	// 		console.log('test happened')
+	// 		return response;
+	// 	})
+	// }),
 	// regions:
 	selectedCountry: null,
 	// selectedRegion: null,
 	
 	actions: {
 		selectCountry: function(selected){
+			Ember.$.ajax({
+				type: "GET",
+      	contentType: "text/xml",
+				url: 'api/countries'
+			}).then(function(response){
+				for(var i=0; i < response.countries.length; i++) {console.log(response.countries[i].name)}
+			})
 			this.set('selectedCountry', selected);
 		},
 		// selectRegion: function(selected){
--- mirage/config.js
+++ mirage/config.js
@@ -9,7 +9,7 @@ export default function() {
   */
 
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
+  this.namespace = '/api';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
   /*
@@ -24,5 +24,12 @@ export default function() {
     http://www.ember-cli-mirage.com/docs/v0.3.x/shorthands/
   */
 
-  this.get('/countries');
+  this.get('/countries', () => {
+    return {
+      countries: [
+        {id: 1, name: "United States"},
+        {id: 2, name: "Canada"}
+      ]
+    };
+  });
 }
