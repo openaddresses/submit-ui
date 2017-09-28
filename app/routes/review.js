6ae2f1d14b973241942a379207170527104b401e
Meghan <mmhade@gmail.com>
2017-09-27 17:08:56 -0700

    remove sample data used for development

--- app/routes/review.js
+++ app/routes/review.js
@@ -2,18 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-    this.store.createRecord('submission', {
-      country: "USA",
-      region: "CA",
-      data_type: "URL",
-      data_link: "www.test.com",
-      source_name: "SF MTA",
-      update_frequency: "Monthly",
-      edit_mode: false,
-      license: {name:"MIT", text: "test text"},
-      maintainer_name: "Thelma",
-      maintainer_email: "thelma@bus.com",
-    });
     return this.get('store').peekAll('submission').get('firstObject');
   }
 });
