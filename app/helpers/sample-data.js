7e19c3fdbfe20ef1c23ca86566f88b86d7d89154
Meghan <mmhade@gmail.com>
2017-10-04 17:56:34 -0700

    pass test by improving helper

--- app/helpers/sample-data.js
+++ app/helpers/sample-data.js
@@ -1,13 +1,18 @@
 import Ember from 'ember';
 
 export function sampleData(params, namedArgs) {
-	var heading = namedArgs.heading
-	var exampleValue = namedArgs.user_data.features[0].properties[heading];
-	if (exampleValue){
-		return exampleValue;
-	} else {
-		return "no example value available for this column"
-	}
+  var heading = namedArgs.heading;
+  var exampleValue;
+  if (namedArgs.user_data){
+    exampleValue = namedArgs.user_data.features[0].properties[heading];
+  }
+  if (namedArgs.user_data && exampleValue){
+    return exampleValue;
+  } else if (namedArgs.user_data) {
+    return "no example value available for this column";
+  } else {
+    return params;
+  }
 }
 
 export default Ember.Helper.helper(sampleData);
