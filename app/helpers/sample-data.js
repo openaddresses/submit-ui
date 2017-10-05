9a6d723c87de9023347c421bec9d5bb66447023c
Meghan <mmhade@gmail.com>
2017-10-04 17:43:44 -0700

    set up helper to show example value in column-slection drop downs

--- app/helpers/sample-data.js
+++ app/helpers/sample-data.js
@@ -1,7 +1,13 @@
 import Ember from 'ember';
 
-export function sampleData(params/*, hash*/) {
-  return params;
+export function sampleData(params, namedArgs) {
+	var heading = namedArgs.heading
+	var exampleValue = namedArgs.user_data.features[0].properties[heading];
+	if (exampleValue){
+		return exampleValue;
+	} else {
+		return "no example value available for this column"
+	}
 }
 
 export default Ember.Helper.helper(sampleData);
