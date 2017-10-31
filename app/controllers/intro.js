d4f861799c224a044cd99ce214161c9376e829b2
Meghan <mmhade@gmail.com>
2017-10-31 16:11:58 -0700

    intro page now asks for link to data or file upload

--- app/controllers/intro.js
+++ app/controllers/intro.js
@@ -4,30 +4,33 @@ import { task, timeout } from 'ember-concurrency';
 import ENV from '../config/environment';
 
 export default Ember.Controller.extend(sharedActions, {
-  country: null,
-  region: null,
-  regionPlaceholder: Ember.computed('country', function(){
-    return "Search for a region in " + this.get('country').properties.label;
-  }),
-  searchCountries: task(function* (term) {
-    yield timeout(167);
-    if (Ember.isBlank(term)) { return []; }
-    const url = `https://search.mapzen.com/v1/autocomplete?api_key=${ENV.searchKey}&layers=country&text=${term}`;      
-    return Ember.$.ajax({ url }).then(json => json.features);
-  }),
-  searchRegions: task(function* (term) {
-    yield timeout(167);
-    if (Ember.isBlank(term)) { return []; }
-    var country = this.get('country').properties.country_a;
-    const url = `https://search.mapzen.com/v1/autocomplete?api_key=${ENV.searchKey}&layers=region&boundary.country=${country}&text=${term}`;      
-    return Ember.$.ajax({ url }).then(json => json.features);
+  dataURL: null,
+  dataFile: null,
+  fileName: Ember.computed('dataFile', function(){
+    if (this.get('dataFile')){
+      return this.get('dataFile').name
+    }
   }),
   actions: {
+    setURL: function(input){
+      this.set('dataFile', null);
+      var url = input.currentTarget.value;
+      this.set('dataURL', url);
+    },
+    uploadFile: function(){
+      this.set('dataURL', null);
+      var file = document.getElementById('uploadfile').files[0];
+      this.set('dataFile', file);
+    },
     changeRoute: function(route){
-      var country = this.get('country').properties.country_a;
-      var region = this.get('region').properties.region_a;
-      // Create new record in store for this submission, with country and region from user input
-      this.store.createRecord('submission', {country: country, region: region});
+      // Create new record in store for this submission, with data link or data file.
+      if (this.get('dataURL')){
+        this.store.createRecord('submission', {data_url: this.get('dataURL')});
+      } else if (this.get('dataFile')){
+        this.store.createRecord('submission', {data_file: this.get('dataFile')});
+      } else {
+        console.log("set up form validation requiring either url or file")
+      }
       this.transitionToRoute(route);
     }
   }
