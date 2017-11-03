0fac682a378108e9974e086afc8d68df61f8047c
Mandy <mandy@mapzen.com>
2017-11-03 15:46:49 -0400

    adding cancel modal; WIP - figure out how to close modal before transitioning to intro

--- app/controllers/review.js
+++ app/controllers/review.js
@@ -4,6 +4,17 @@ import sharedActions from '../mixins/shared-actions';
 
 export default Ember.Controller.extend(sharedActions, {
   actions: {
+    openModal: function(name) {
+      $('.ui.' + name + '.modal').modal('show');
+    },
+    approveModal: function(element, component) {
+      this.get('store').unloadAll('submission');
+      this.transitionToRoute("intro");
+      return true;
+    },
+    denyModal: function(element, component) {
+      return true;
+    },
     changeRoute: function(route){
       this.transitionToRoute(route);
     },
@@ -13,10 +24,6 @@ export default Ember.Controller.extend(sharedActions, {
     submit: function(){
       // communicate with backend here
       this.transitionToRoute("success");
-    },
-    cancel: function(){
-      this.get('store').unloadAll('submission');
-      this.transitionToRoute("intro");
     }
   }
 });
