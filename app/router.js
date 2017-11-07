da3e45ace5b74b6055ac3ce3baa2beb0f86d8b92
Meghan <mmhade@gmail.com>
2017-11-07 09:33:11 -0800

    destroy license route, controller, tests

--- app/router.js
+++ app/router.js
@@ -8,7 +8,6 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('source');
-  this.route('license');
   this.route('mapping', function() {});
   this.route('contact');
   this.route('success');
