30d26a14de03fc2b6944d5e95d3340498350562b
Mandy <mandy@mapzen.com>
2017-11-10 10:38:02 -0500

    ignoring jquery use for modal; not sure if ideal

--- app/controllers/review.js
+++ app/controllers/review.js
@@ -5,14 +5,18 @@ import sharedActions from '../mixins/shared-actions';
 export default Ember.Controller.extend(sharedActions, {
   actions: {
     openModal: function(name) {
+      /*eslint-disable */
       $('.ui.' + name + '.modal').modal('show');
+      /*eslint-enable */
     },
     approveModal: function(element, component) {
+      /*eslint-disable */
       $('.ui.modal').modal('toggle', element, component);
+      /*eslint-enable */
       this.get('store').unloadAll('submission');
       this.transitionToRoute('intro');
     },
-    denyModal: function(element, component) {
+    denyModal: function() {
       return true;
     },
     changeRoute: function(route){
