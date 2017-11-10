d41a83dbb2c2d57da6c5ac7c55563162c58ebcbd
Meghan <mmhade@gmail.com>
2017-11-09 16:25:22 -0800

    use webServiceResponse to set sample row information on submission model

--- app/controllers/data-format.js
+++ app/controllers/data-format.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  columns: null,
+  columnHeadings: Ember.computed('model.webServiceResponse', function(){
+    return Object.keys(this.model.webServiceResponse.responses[0].properties);
+  }),
+  actions: {
+    chooseColumn: function(heading, column){
+      Ember.set(this.model.submission.get('oaFields')[heading], "columns", []);
+      this.model.submission.get('oaFields')[heading].columns[0] = column;
+      for (var i = 0; i < 2; i++){
+        Ember.set(this.model.submission.get('exampleRows')[i], heading, this.model.webServiceResponse.responses[i].properties[column]);
+      }
+    },
+  }
 });
