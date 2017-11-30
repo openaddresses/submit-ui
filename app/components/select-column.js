5ee5ae987504e76411262803f138580db4701a5d
Mandy Kong <mk5506@nyu.edu>
2017-11-30 15:41:17 -0500

    merging in master

---
+++ app/components/select-column.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  field: null,
+  columnHeadings: null,
+  user_data: Ember.computed('model.webServiceResponse', function(){
+    return this.model.webServiceResponse.source_data.results;
+  }),
+  actions: {
+    sendChooseField: function(heading, column){
+      Ember.set(this.model.submission.get('oaFields')[heading], "fields", []);
+      this.model.submission.get('oaFields')[heading].fields.addObject(column);
+      for (var i = 0; i < 2; i++){
+        Ember.set(this.model.submission.get('exampleRows')[i], heading, [this.model.webServiceResponse.source_data.results[i][column]]);
+      }
+    }
+  }
+});
