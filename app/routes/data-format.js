dc1ac1f9e2632d21cef75d2ff08d8e2c7d273a1c
Meghan <mmhade@gmail.com>
2017-12-05 14:40:06 -0800

    dropdowns for joins now only show remaining options

--- app/routes/data-format.js
+++ app/routes/data-format.js
@@ -7,13 +7,13 @@ export default Ember.Route.extend({
   },
   model: function(){
     // for development, remove before merging
-    this.store.createRecord('submission');
+    // this.store.createRecord('submission');
     
     
     var submission = this.get('store').peekAll('submission').get('firstObject');
     // request will change once app is set up to work with web service
-    var request = submission.data_url;
-    // var request = 'api/responses';
+    // var request = submission.data_url;
+    var request = 'api/responses';
 
     var webServiceResponse = this.getWebServiceResponse(request);
       
