0e703af58ce327af0d83adf28161aed112a37985
Meghan <mmhade@gmail.com>
2017-12-06 11:14:54 -0800

    Switch function back to switch if prefix/postfix is removed

--- app/components/remove-prefix-postfix.js
+++ app/components/remove-prefix-postfix.js
@@ -23,7 +23,7 @@ export default Ember.Component.extend({
       }
     },
     removeFunction: function(field){
-      Ember.set(this.model.submission.get('oaFields')[field], "function", null);
+      Ember.set(this.model.submission.get('oaFields')[field], "function", "split");
       for (var i = 0; i < 2; i++){
         var originalColumn = this.model.submission.get('oaFields')[field].fields[0]
         Ember.set(this.model.submission.get('exampleRows')[i], field, [this.model.webServiceResponse.source_data.results[i][originalColumn]]);
