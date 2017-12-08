1e831429a7878bc4c1cb43a982aa9a965fffc2fc
Mandy <mandy@mapzen.com>
2017-12-08 11:43:02 -0500

    fixed modal so that when transitioning to next page it does not render two modals

--- app/components/help-modal.js
+++ app/components/help-modal.js
@@ -1,20 +1,57 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  dataURL: null,
+  dataFile: null,
+  fileName: Ember.computed('dataFile', function(){
+    if (this.get('dataFile')){
+      return this.get('dataFile').name
+    }
+  }),
+  email: null,
+  text: null,
   actions: {
     openModal: function(name) {
       /*eslint-disable */
-      $('.ui.' + name + '.modal').modal('show');
+      $('.ui.' + name + '.modal').modal('hide others');
+      $('.ui.' + name + '.modal').modal({
+        observeChanges: true,
+      }).modal('show');
+      return false;
       /*eslint-enable */
     },
     submitModal: function(element, component) {
       /*eslint-disable */
       $('.ui.modal').modal('toggle', element, component);
+
+      /* TODO: create issue with collected information */
+
       this.get('submit')("success");
       /*eslint-enable */
     },
-    cancelModal: function() {
+    cancelModal: function(name) {
+      $('.ui.' + name + '.modal').modal('hide');
       return true;
+    },
+    setEmail: function(input) {
+      this.set('email', null);
+      var email = input.currentTarget.value;
+      this.set('email', email);
+    },
+    setText: function(text) {
+      this.est('text', null);
+      var text = input.currentTarget.value;
+      this.set('text', text);
+    },
+    uploadFile: function(){
+      this.set('dataURL', null);
+      var file = document.getElementById('uploadSource').files[0];
+      this.set('dataFile', file);
+    },
+    setURL: function(input){
+      this.set('dataFile', null);
+      var url = input.currentTarget.value;
+      this.set('dataURL', url);
     }
   }
 });
