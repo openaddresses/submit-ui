1d0e208ad19b6164c9cf15935d5c298a983c771b
Hanbyul Jo <hanbyul@nyu.edu>
2017-12-08 21:48:02 -0500

    created record plain

--- app/routes/intro.js
+++ app/routes/intro.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-     var submission = this.store.createRecord('submission', {data_url: ''});
+     var submission = this.store.createRecord('submission');
     return submission;
   }
 });
