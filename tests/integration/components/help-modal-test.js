5f547487d34cd68a1f75b07e0e35d5d79e76e1c5
Mandy <mandy@mapzen.com>
2017-12-13 10:20:28 -0500

    including run function for integration test

--- tests/integration/components/help-modal-test.js
+++ tests/integration/components/help-modal-test.js
@@ -1,5 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import { run } from '@ember/runloop';
 
 moduleForComponent('help-modal', 'Integration | Component | help modal', {
   integration: true
