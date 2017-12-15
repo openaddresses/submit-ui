156ebdd843887e516f042949b30ac070e70a53b9
Hanbyul Jo <hanbyul@nyu.edu>
2017-12-15 10:26:43 -0500

    implemented basic requirement for more-info data validation

--- app/controllers/more-info.js
+++ app/controllers/more-info.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
+import LicenseValidator from '../validator/license'
+
 export default Ember.Controller.extend({
+  LicenseValidator,
+  showErrorState: false,
+  errorMessages: [],
+  license: '',
   licenses: [{
     name: "Open Data Commons Attribution License (ODC-By)",
     url: "http://opendatacommons.org/licenses/by/1.0/"
@@ -18,6 +24,49 @@ export default Ember.Controller.extend({
     url: "http://www.gnu.org/licenses/fdl-1.3.en.html"
   }],
   frequencies: ["daily", "weekly", "monthly", "quarterly", "annually", "Unknown"],
+  checkFormErrors: function (changeset, keyName) {
+    const val = (this.model.get(keyName))? this.model.get(keyName): '';
+    changeset.set(keyName, val);
+    return new Promise((resolve, reject) => {
+      changeset.validate().then(()=> {
+          console.log(changeset.get('errors'));
+          resolve(changeset.get('errors')
+                          .filter ((e) => e.key === keyName)
+                          .reduce((errorMessages, error) => {
+                            console.log(errorMessages);
+                            error.validation.map((validationMessage) => {
+                              errorMessages.push(validationMessage)
+                            })
+                            return errorMessages;
+                          }, []));
+        })
+      .catch((err) => {reject(err)})
+    })
+  },
+  checkErrors: function (changeset) {
+    // return this.checkFormErrors(changeset, 'attribution_text');
+    if (this.model.get('license') === 'provide' && !this.model.get('attribution')) return this.checkFormErrors(changeset, 'user_submitted_url');
+    if (this.model.get('license') !== 'provide' && this.model.get('attribution')) return this.checkFormErrors(changeset, 'attribution_text');
+    if (this.model.get('license') === 'provide' && this.model.get('attribution')) {
+        let msgs = [];
+        return this.checkFormErrors(changeset, 'user_submitted_url')
+        .then((errorMessages) => {
+          errorMessages.map((error) => msgs.push(error))
+        })
+        .then(() => this.checkFormErrors(changeset, 'attribution_text'))
+        .then((errorMessages) => {
+          console.log(errorMessages);
+          errorMessages.map((error) => msgs.push(error))
+          return msgs;
+        })
+      }
+
+    if (this.model.get('license') !== 'provide' && !this.model.get('attribution')) return new Promise(resolve => resolve([]))
+  },
+  resetErrorState: function () {
+    Ember.set(this, 'showErrorState', false);
+    Ember.set(this, 'errorMessages', []);
+  },
   actions: {
     licenseExists: function(input){
       this.model.set('license_exists', input)
@@ -33,11 +82,27 @@ export default Ember.Controller.extend({
       this.model.set('license', license.name)
       this.model.set('license_url', license.url)
     },
-    changeRoute: function(route){
-      this.transitionToRoute(route);
+    changeRoute: function(route, changeset){
+      this.checkErrors(changeset)
+        .then((errorMsgs) => {
+          // When there is any error, show it and do not proceed
+          if (errorMsgs.length) {
+            Ember.set(this, 'showErrorState', true);
+            Ember.set(this, 'errorMessages', errorMsgs);
+          // When there is no error message, proceed
+          } else {
+            this.resetErrorState();
+            this.transitionToRoute(route);
+          }
+          })
+        .catch ((err) => {
+          const errorMsgs = [err];
+          Ember.set(this, 'showErrorState', true);
+          Ember.set(this, 'errorMessages', errorMsgs);
+        })
     },
     userSubmittedLicense: function(input){
-      this.model.set('license', null)
+      // this.model.set('license', 'provide')
       this.model.set('license_url', null)
       this.model.set('user_submitted_url', input.target.value)
     },
